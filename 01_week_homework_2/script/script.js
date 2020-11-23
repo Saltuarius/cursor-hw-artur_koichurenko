0d5d1de8af0db79ee4fa20663440d0d3fa7bae5d
Artur Koichurenko <saltuarius29@gmail.com>
2020-11-23 23:52:53 +0200

    Add files via upload

--- 01_week_homework_2/script/script.js
+++ 01_week_homework_2/script/script.js
@@ -3,18 +3,18 @@
 let inputFirstNumber = +prompt("Введіть перше ціле число!");
 inputFirstNumber = Math.trunc(inputFirstNumber);
 
-while (inputFirstNumber === " " || isNaN(inputFirstNumber))  
+while (!inputFirstNumber)
 {  
-    inputFirstNumber = prompt("Е-ні! Введіть ціле число!");  
+    inputFirstNumber = +prompt("Е-ні! Введіть ціле число!");  
 }  
 alert(`Ваше перше число: ${inputFirstNumber}`);
 
 let inputLastNumber = +prompt("Введіть кінцеве ціле число!");
 inputLastNumber = Math.trunc(inputLastNumber);
 
-while (inputLastNumber === " " || isNaN(inputLastNumber) || inputFirstNumber > inputLastNumber)  
+while (!inputLastNumber || inputFirstNumber > inputLastNumber)  
 {  
-    inputLastNumber = prompt("Е-ні! Введіть ціле число, яке більше від попереднього!");  
+    inputLastNumber = +prompt("Е-ні! Введіть ціле число, яке більше від попереднього!");  
 }  
 alert(`Ви ввели число ${inputLastNumber}`);
 
